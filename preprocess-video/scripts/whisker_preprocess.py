3ebb246d546c26a88d89bf153bb3c3bda20e93c7
Graham Voysey <gvoysey@bu.edu>
2017-02-17 14:46:57 -0500

    broken opencv start

--- preprocess-video/scripts/whisker_preprocess.py
+++ preprocess-video/scripts/whisker_preprocess.py
@@ -25,6 +25,7 @@ import sys
 from logging import info, error, getLogger, ERROR
 from os import access, W_OK, utime
 
+import progressbar
 import cv2
 from attrs_utils.interop import from_docopt
 import attr
@@ -33,6 +34,7 @@ from core.base import *
 from core.eye_blink import EyeBlink
 from core.whisker_motion import WhiskerMotion
 import core.yaml_config as yaml_config
+import numpy as np
 
 
 def main(inputargs):
@@ -49,19 +51,54 @@ def main(inputargs):
     info('read default hardware parameters.')
     info('processing file {0}'.format(path.split(args.input)[1]))
 
-    # reduce to 8-bit greyscale
-    cv2.VideoCapture(args.input)
-    # invert colors
+    # preprocess video
 
-    # split left and right
+    outname = 'foo.mp4'
+    cap = cv2.VideoCapture(args.input)
 
-    # handle framelength or re-align frame times (tbd)
+    # codec = cv2.VideoWriter_fourcc(*'H264')
+    codec = int(cap.get(cv2.CAP_PROP_FOURCC))
+    framerate = app_config.camera.framerate
+    size = (app_config.camera.width, app_config.camera.height)
+
+    vw = cv2.VideoWriter(filename=outname, fourcc=codec, fps=framerate, frameSize=size, isColor=False)
+    curframe = 0
+    with progressbar.ProgressBar(min_value=0, max_value=int(cap.get(cv2.CAP_PROP_FRAME_COUNT))) as pb:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if ret:
+                curframe += 1
+                pb.update(curframe)
+                # convert to greyscale
+                grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                # invert colors
+                inverted = cv2.bitwise_not(grey)
+                vw.write(inverted)
+                # crop left half
+                half_width = round(app_config.camera.width)
+                #left = inverted[1:app_config.camera.height, 1:half_width]
+                #cv2.imshow('left', left)
+                #right = inverted[1:app_config.camera.height, half_width: app_config.camera.width]
+                #vw.write(inverted)
+
+
+                #cv2.imshow('right', right)
+                #if cv2.waitKey(1) & 0xFF == ord('q'):
+                #    break
+            else:
+                break
+
+        cap.release()
+        vw.release()
+        cv2.destroyAllWindows()
+
+
 
     # extract whisking data for left and right
-    (leftw, rightw) = WhiskerMotion(infile=args.input, outfile=args.output,
-                                    camera_params=app_config.camera).extract_all()
-    (lefte, righte) = EyeBlink(infile=args.input, outfile=args.output,
-                               camera_params=app_config.camera).extract_all()
+    # (leftw, rightw) = WhiskerMotion(infile=args.input, outfile=args.output,
+    #                                 camera_params=app_config.camera).extract_all()
+    # (lefte, righte) = EyeBlink(infile=args.input, outfile=args.output,
+    #                            camera_params=app_config.camera).extract_all()
 
     # test_serialized('test.json', camera_parameters)
     # Return whisker data from file.
