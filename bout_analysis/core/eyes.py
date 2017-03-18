f7e84f9040fb2bb38927cfe9d51f9f6aff57091b
Graham Voysey <gvoysey@bu.edu>
2017-03-18 11:16:45 -0400

    name refactoring

---
+++ bout_analysis/core/eyes.py
@@ -0,0 +1,48 @@
+import cv2
+import numpy as np
+
+
+def __show_debug_image(name, frame):
+    return
+    cv2.imshow(name, frame)
+    cv2.waitKey()
+
+
+def compute_areas(frame):
+    frame_hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    __show_debug_image('original', frame_hsv)
+    lower_red = np.array([0, 50, 50])
+    upper_red = np.array([10, 255, 255])
+    mask0 = cv2.inRange(frame_hsv, lower_red, upper_red)
+    lower_red1 = np.array([170, 50, 50])
+    upper_red1 = np.array([180, 255, 255])
+    mask1 = cv2.inRange(frame_hsv, lower_red1, upper_red1)
+    mask = mask0 + mask1
+    output_img = frame_hsv.copy()
+    output_img[np.where(mask == 0)] = 0
+    output_grey = cv2.cvtColor(output_img, cv2.COLOR_BGR2GRAY)
+
+    # output_grey = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    # _, _, output_grey = cv2.split(frame)
+
+    __show_debug_image('grey', output_grey)
+    ret, thresh1 = cv2.threshold(output_grey, 150, 255,
+                                 cv2.THRESH_OTSU)
+    __show_debug_image('thresholded', thresh1)
+    # the value of 15 is chosen by trial-and-error to produce the best outline of the skull
+    kernel = np.ones((2, 2), np.uint8)
+    # square image kernel used for erosion
+    erosion = cv2.erode(thresh1, kernel, iterations=2)
+    # refines all edges in the binary image
+
+    opening = cv2.morphologyEx(erosion, cv2.MORPH_OPEN, kernel)
+    closing = cv2.morphologyEx(opening, cv2.MORPH_CLOSE,
+                               kernel)  # this is for further removing small noises and holes in the image
+    # todo: improve this to join large blobs (in case of eyebrow occlusion)
+    __show_debug_image('processed', closing)
+    return closing.size, np.count_nonzero(closing)
+
+
+if __name__ == "__main__":
+    this_frame = cv2.imread('first.png')
+    compute_areas(this_frame)
---
+++ bout_analysis/core/whiskers.py
@@ -0,0 +1,48 @@
+from collections import namedtuple
+from os import path
+
+import matplotlib
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from matplotlib.backends.backend_pdf import PdfPages
+
+from scripts.analyze_bout import VideoFileData
+from .util.filters import butter_bandpass_filter
+from .yaml_config import Config
+
+plt.style.use('fivethirtyeight')
+matplotlib.use('PDF')
+sns.set(color_codes=True)
+
+timedata = namedtuple("timedata", "frameid,mean_degrees,num_whiskers,stderr")
+
+
+def plot_left_right(results: [VideoFileData]):
+    with PdfPages(filename=fp) as pdf:
+        ax = left.plot.line(x='time', y='mean_degrees_filtered')
+        right.plot.line(ax=ax, x='time', y='mean_degrees_filtered')
+        fig = ax.get_figure()
+        fig.dpi = 400
+        fig.figsize = (8.5, 11)
+        pdf.savefig(fig)
+
+
+def plot(dirpath, df):
+    with PdfPages(filename=path.join(dirpath, df.name + ".pdf")) as pdf:
+        ax = df.plot.line(x='frameid', y='mean_degrees', yerr='stderr')
+        fig = ax.get_figure()
+        fig.dpi = 400
+        fig.figsize = (8.5, 11)
+        pdf.savefig(fig)
+
+
+def filter_raw(whiskdat: pd.DataFrame, params: Config, name: str) -> pd.DataFrame:
+    low = 2
+    high = 50
+    fs = params.camera.framerate
+
+    whiskdat = whiskdat.assign(mean_degrees_filtered=butter_bandpass_filter(whiskdat['mean_degrees'], low, high, fs))
+    whiskdat = whiskdat.assign(time=whiskdat['frameid'] / fs)
+    whiskdat.name = name
+    return whiskdat
