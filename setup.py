2ee8f6f7555c8954d3040b0f58016c54cf16cfa0
Graham Voysey <gvoysey@bu.edu>
2017-03-19 20:08:45 -0400

    updated setup

--- setup.py
+++ setup.py
@@ -1,15 +1,19 @@
 from setuptools import setup, find_packages, Extension
 # makes __version__ a local variable
-exec(open('bout_analysis/core/_version.py').read())
+exec(open('mousetracker/core/_version.py').read())
 setup(
     name='mousetracker',
     version=__version__,
     packages=find_packages(),
-    scripts=['bout_analysis/scripts/analyze_bout.py',
+    scripts=['mousetracker/scripts/analyze_bout.py',
             ],
     url='https://github.com/MEEI-SPEL/mousetracker',
     license='',
     author='Graham Voysey',
     author_email='gvoysey@bu.edu',
-    description=''
+    description='',
+    install_requires=['opencv_python',
+                      'pandas',
+                      'progressbar2',
+                      'joblib']
 )
