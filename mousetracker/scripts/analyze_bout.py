12d04667c616701d7e97079f974002dcfb913965
Graham Voysey <gvoysey@bu.edu>
2017-03-23 10:45:22 -0400

    imports

--- mousetracker/scripts/analyze_bout.py
+++ mousetracker/scripts/analyze_bout.py
@@ -33,8 +33,7 @@ import progressbar
 from attrs_utils.interop import from_docopt
 from joblib import Parallel, delayed
 
-from core import eyes
-import core.yaml_config as yaml_config
+from core import eyes, yaml_config
 from core._version import __version__
 from core.base import *
 from core.whiskers import extract_whisk_data
