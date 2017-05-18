e9d43d5fa188d84e0096c455b002d94a26223796
Graham Voysey <gvoysey@bu.edu>
2017-05-18 14:24:48 -0400

    Cleaned up the config file to require fewer parameters.
    Updated template and default yaml files to reflect new changes.
    
    Added more sanity checks to __check_requirements() to enforce the same system requirements that were previously handled in the config file.
    
    Changed ffmpeg call to use the version on the path.

--- mousetracker/core/yaml_config.py
+++ mousetracker/core/yaml_config.py
@@ -5,20 +5,18 @@ import attr
 import yaml
 from attr.validators import instance_of
 from attrs_utils import ensure_cls, ensure_enum, is_path_of_file
-from .base import modulePath
+from mousetracker.core.base import modulePath
 
 Species = Enum('Species', "mouse, rat")
 WhiskColor = Enum('WhiskColor', 'white, black')
 EyeColor = Enum('EyeColor', 'red')
 
 
-def low_smaller_than_high(instance, attribute, value):
-    if value >= instance.high:
-        raise ValueError("'low' has to be smaller than 'high'!")
-
-
 @attr.s(frozen=True)
 class Camera(object):
+    """
+    Data container for parameters relating to the camera recording the animal
+    """
     width = attr.ib(validator=instance_of(int))
     height = attr.ib(validator=instance_of(int))
     framerate = attr.ib(validator=instance_of(int))
@@ -27,6 +25,9 @@ class Camera(object):
 
 @attr.s(frozen=True)
 class Animal(object):
+    """
+    Data about the animal
+    """
     species = attr.ib(convert=ensure_enum(Species))
     whisker_color = attr.ib(convert=ensure_enum(WhiskColor))
     eye_color = attr.ib(convert=ensure_enum(EyeColor))
@@ -35,21 +36,18 @@ class Animal(object):
 
 @attr.s(frozen=True)
 class System(object):
+    """
+    System parameters
+    """
     python27_path = attr.ib(validator=is_path_of_file)
     trace_path = attr.ib(validator=is_path_of_file)
-    avidemux_path = attr.ib(validator=is_path_of_file)
-    ffmpeg_path = attr.ib(validator=is_path_of_file)
-    whisk_base_path = attr.ib(validator=instance_of(str))
-
-
-@attr.s(frozen=True)
-class Bandpass(object):
-    low = attr.ib(validator=low_smaller_than_high)
-    high = attr.ib(validator=instance_of(int))
 
 
 @attr.s(frozen=True)
 class Storage(object):
+    """
+    Storage parameters
+    """
     root_label = attr.ib(validator=instance_of(str))
     output_root = attr.ib(validator=instance_of(str))
     name_format = attr.ib(validator=instance_of(str))
@@ -57,10 +55,12 @@ class Storage(object):
 
 @attr.s(frozen=True)
 class Config:
+    """
+    All configuration values
+    """
     camera = attr.ib(convert=ensure_cls(Camera))
     animal = attr.ib(convert=ensure_cls(Animal))
     system = attr.ib(convert=ensure_cls(System))
-    bandpass = attr.ib(convert=ensure_cls(Bandpass))
     storage = attr.ib(convert=ensure_cls(Storage))
 
 
